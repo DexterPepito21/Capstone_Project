9163a4d9a34634d2d15e2b88c7ea32a5b2374308
CamelleAnnBasilio <basiliocamelleann.18@gmail.com>
2021-10-19 22:41:17 +0800

    sms code

--- admin_sms.php
+++ admin_sms.php
@@ -2,9 +2,51 @@
 session_start();
 
 include("php/connection.php");
-include("php/functions.php");
+include("php/admin_functions.php");
 
 $user_data = check_login($con);
+
+//##########################################################################
+// ITEXMO SEND SMS API - PHP - CURL-LESS METHOD
+// Visit www.itexmo.com/developers.php for more info about this API
+//##########################################################################
+function itexmo($number,$message,$apicode,$passwd){
+    $url = 'https://www.itexmo.com/php_api/api.php';
+    $itexmo = array('1' => $number, '2' => $message, '3' => $apicode, 'passwd' => $passwd);
+    $param = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($itexmo),
+        ),
+    );
+    $context  = stream_context_create($param);
+    return file_get_contents($url, false, $context);
+}
+//##########################################################################
+
+if($_POST){
+    $number = $_POST['number'];
+    $name = $_POST['name'];
+    $msg = $_POST['msg'];
+    $api = "TR-CAMEL935366_PTYY2";
+    $pass = "kpdz9}}!9]";
+    $text = $name.":   ".$msg;
+
+    if(!empty($_POST['name']) && ($_POST['number']) && ($_POST['msg'])){
+$result = itexmo($number,$text,$api,$pass);
+    if ($result == ""){
+    echo "iTexMo: No response from server!!!
+    Please check the METHOD used (CURL or CURL-LESS). If you are using CURL then try CURL-LESS and vice versa.	
+    Please CONTACT US for help. ";	
+    }else if ($result == 0){
+    echo "Message Sent!";
+    }
+    else{	
+    echo "Error Num ". $result . " was encountered!";
+    }
+}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,6 +57,7 @@ $user_data = check_login($con);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/admin_sms.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.js"></script>
     
 </head>
@@ -36,44 +79,43 @@ $user_data = check_login($con);
           <div class="dropdown">
             <button class="dropbtn"><i class="fa fa-caret-down"></i></button>
             <div class="dropdown-content">
-            <a href="logout.php"><i class="fas fa-sign-out-alt" id="icon"></i>Logout</a>
+            <a href="php/logout.php"><i class="fas fa-sign-out-alt" id="icon"></i>Logout</a>
             </div>
           </div>
         </ul>
       </nav>
 
-<div class="content" style="overflow-x: auto;">
+<div class="content">
     <select class="sms-options toShow" name="status" id="status">
         <option value="1">Single or Multiple SMS</option>
         <option value="0">Scheduled SMS</option>
     </select>
-    <div class="sms-notification">
-        <form class="my-form">
-            <div class="form-group">
-                <label>Username:</label>
-                <input type="text" name="name" required>
-            </div>
-            <div class="form-group">
-                <label>Sender:</label>
-                <input type="text" name="name" required>
-            </div>
-            <div class="form-group">
-                <label>Number:</label>
-                <input type="number" data-maxlength="11" oninput="this.value=this.value.slice(0,this.dataset.maxlength)" required/>
-            </div>
-            <div class="form-group">
-                <label class="message">Message:</label>
-                <textarea name="message" cols="40" rows="10" required></textarea>
-            </div>
-            <div class="form-group toShowSched" style="display: none;">
+    <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 col-xs-12">
+                    <form action="admin_sms.php" method="POST">
+                        <div class="form-group">
+                            <label for="name">Your Name</label>
+                            <input type="text" maxlength="15" class="form-control" id="name" placeholder="Name" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="number">Recipient's Mobile Number</label>
+                            <input type="text" maxlength="11" class="form-control" id="number" placeholder="Mobile Number" name="number" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="msg">Your Message</label>
+                            <textarea class="form-control" rows="3" name="msg" placeholder="Message here" onkeyup="countChar(this)" required></textarea>
+                        </div>
+                        <!-- <div class="form-group toShowSched" style="display: none;">
                 <label>Scheduled At:</label>
                 <input type="date" class="date" name="date" required>
+            </div> -->
+                        <p class="text-right" id="charNum">100</p>
+                        <button type="submit" class="btn btn-success">Send</button>
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-                <input type="submit" value="Send">
-            </div>
-        </form>
-    </div>
+        </div>
 
 </div>
 </div>
@@ -117,5 +159,20 @@ $user_data = check_login($con);
         });
         
     </script>
+
+    <!-- sms -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+        <script>
+            function countChar(val){
+                var len = val.value.length;
+                if (len >=85){
+                    val.value = val.value.substring(0,85);
+                }else{
+                    $('#charNum').text(85-len);
+                }
+            };
+        </script>
 </body>
 </html>
