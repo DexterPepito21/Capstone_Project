d6a03b4062ca65e000dd3018782069683d0e854e
CamelleAnnBasilio <79122695+CamelleAnnBasilio@users.noreply.github.com>
2021-11-13 20:05:19 +0800

    sms code

--- admin_sms.php
+++ admin_sms.php
@@ -5,6 +5,48 @@ include("php/connection.php");
 include("php/functions.php");
 
 $user_data = check_login($con);
+
+//##########################################################################
+// ITEXMO SEND SMS API - PHP - CURL-LESS METHOD
+// Visit www.itexmo.com/developers.php for more info about this API
+//##########################################################################
+function itexmo($number,$message,$apicode,$passwd){
+    $url = 'https://www.itexmo.com/php_api/api.php';
+    $itexmo = array('1' => $number, '2' => $message, '3' => $apicode, 'passwd' => $passwd);
+    $param = array(
+        'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($itexmo),
+        ),
+    );
+    $context  = stream_context_create($param);
+    return file_get_contents($url, false, $context);
+}
+//##########################################################################
+
+if($_POST){
+    $number = $_POST['number'];
+    $name = $_POST['name'];
+    $msg = $_POST['msg'];
+    $api = "TR-PATPE797790_QMKWB";
+    $pass = "2[)ewnm4cn";
+    $text = $name.":   ".$msg;
+
+    if(!empty($_POST['name']) && ($_POST['number']) && ($_POST['msg'])){
+$result = itexmo($number,$text,$api,$pass);
+    if ($result == ""){
+    echo "iTexMo: No response from server!!!
+    Please check the METHOD used (CURL or CURL-LESS). If you are using CURL then try CURL-LESS and vice versa.	
+    Please CONTACT US for help. ";	
+    }else if ($result == 0){
+    echo "Message Sent!";
+    }
+    else{	
+    echo "Error Num ". $result . " was encountered!";
+    }
+}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,11 +55,11 @@ $user_data = check_login($con);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/admin_sms.css">
+    <link rel="stylesheet" href="./css/admin_sms.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.3.1/jszip-2.5.0/dt-1.10.22/b-1.6.5/b-colvis-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/datatables.min.css"/>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
 </head>
 <body>
       <!-- Navigation Bar -->
@@ -29,8 +71,9 @@ $user_data = check_login($con);
         <label class="logo">Child Care System</label>
         <ul>
           <li><a href="admin_index.php"><i class="fas fa-home" id="icon"></i>Dashboard</a></li>
-          <li><a href="admin_input.php"><i class="fas fa-book"  id="icon"></i>Input</a></li>
+          <li><a href="admin_child.php"><i class="fas fa-child"  id="icon"></i>Child Profile</a></li>
           <li><a href="admin_chart.php"><i class="fa fa-chart-bar"  id="icon"></i>Vaccine Chart</a></li>
+          <li><a href="admin_guide.php"><i class="fas fa-book"  id="icon"></i>Nutrition Guide</a></li>
           <li><a href="admin_sms.php" class="active"><i class="fas fa-comment"  id="icon"></i>SMS Notification</a></li>
         
           <div class="dropdown">
@@ -38,54 +81,46 @@ $user_data = check_login($con);
             <div class="dropdown-content">
             <a href="php/logout.php"><i class="fas fa-sign-out-alt" id="icon"></i>Logout</a>
             </div>
+            
           </div>
         </ul>
       </nav>
 
 
-      <i class="fa fa-arrow-circle-right"></i><a href="http://sms.onewaysms.ph/" class="link">Click this to send SMS Notification</a>
+                    <center><form action="admin_sms.php" method="POST" style="width: 30%">
+                        <div class="form-group">
+                            <label for="name">Your Name</label>
+                            <input type="text" maxlength="15" class="form-control" id="name" placeholder="Name" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="number">Recipient's Mobile Number</label>
+                            <input type="text" maxlength="11" class="form-control" id="number" placeholder="Mobile Number" name="number" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="msg">Your Message</label>
+                            <textarea class="form-control" rows="3" name="msg" placeholder="Message here" onkeyup="countChar(this)" required></textarea>
+                        </div>
+                        <!-- <div class="form-group toShowSched" style="display: none;">
+                <label>Scheduled At:</label>
+                <input type="date" class="date" name="date" required>
+            </div> -->
+                        <p class="text-right" id="charNum">100</p>
+                        <button type="submit" class="btn btn-success">Send</button>
+                    </form></center>
+   
+    <!-- sms -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
 
-
-<center><div class="container">
-<table id="example" class="display nowrap" style="width:50%">
-        <thead>
-            <tr>
-                <th>Contact Number</th>
-                <th>Names</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-            $sql = "SELECT * FROM parent_tbl";
-            $stmt = $con->prepare($sql);
-            $stmt->execute();
-            $result = $stmt->get_result();
-            while($row = $result->fetch_assoc()){
-            ?>
-            <tr>
-            <td><?php echo $row['phonenum']; ?></td>
-            <td><?php echo $row['firstname'].'  '.$row['lastname']; ?></td>   
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-</div>
-</center>
-
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.3.1/jszip-2.5.0/dt-1.10.22/b-1.6.5/b-colvis-1.6.5/b-flash-1.6.5/b-html5-1.6.5/b-print-1.6.5/datatables.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-<script text="text/javascript">
-$(document).ready(function() {
-    $('#example').DataTable( {
-        dom: 'Blfrtip',
-        buttons: [
-            'copy', 'csv'
-        ]
-    } );
-} );
-</script>
-    </body>
+        <script>
+            function countChar(val){
+                var len = val.value.length;
+                if (len >=85){
+                    val.value = val.value.substring(0,85);
+                }else{
+                    $('#charNum').text(85-len);
+                }
+            };
+        </script>
+</body>
 </html>
