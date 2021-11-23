4718e185870f08a20239c6cf39cb25bdf041ec8f
CamelleAnnBasilio <79122695+CamelleAnnBasilio@users.noreply.github.com>
2021-11-23 14:50:05 +0800

    Updated files for mobile view

--- admin_input.php
+++ admin_input.php
@@ -56,8 +56,6 @@ if(isset($_POST['guide'])){
             <li><a href="admin_index.php" ><i class="fas fa-home" id="icon"></i>Dashboard</a></li>
             <li><a href="admin_input.php" class="active"><i class="fas fa-book"  id="icon"></i>Input</a></li>
             <li><a href="admin_chart.php"><i class="fa fa-chart-bar"  id="icon"></i>Vaccine Chart</a></li>
-            <li><a href="admin_sms.php"><i class="fas fa-comment"  id="icon"></i>SMS Notification</a></li>
-          
             <div class="dropdown">
               <button class="dropbtn"><i class="fa fa-caret-down"></i></button>
               <div class="dropdown-content">
