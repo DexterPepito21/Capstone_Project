1968a05fcc1e4b8d4a626af5f1ef5134d4277169
CamelleAnnBasilio <79122695+CamelleAnnBasilio@users.noreply.github.com>
2021-11-24 19:50:05 +0800

    add notification

--- parent_index.php
+++ parent_index.php
@@ -15,9 +15,14 @@ $user_data = check_login($con);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/parent_index.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- notif -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <title>Dashboard | Parent</title>
 </head>
 <body>
+
     <!-- Navigation Bar -->
     <nav>
         <input type="checkbox" id="check">
@@ -30,7 +35,12 @@ $user_data = check_login($con);
           <li><a href="parent_child.php"><i class="fas fa-child"  id="icon"></i>Child Profile</a></li>
           <li><a href="parent_chart.php"><i class="fa fa-chart-bar"  id="icon"></i>Vaccine Chart</a></li>
           <li><a href="parent_guide.php"><i class="fas fa-book"  id="icon"></i>Nutrition Guide</a></li>
-                  
+
+      <li class="dropdown">
+       <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-envelope" style="font-size:18px;"></span></a>
+       <ul class="dropdown-menu"></ul>
+      </li>
+      
           <div class="dropdown">
             <button class="dropbtn"><i class="fa fa-caret-down"></i></button>
             <div class="dropdown-content">
@@ -70,8 +80,49 @@ $user_data = check_login($con);
         </div>
         </div>
 
-    
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</body>
+
+    <!-- notif -->
+    <script>
+$(document).ready(function(){
+
+var parent_id = '<?=$parent_id?>';
+console.log(parent_id); 
+
+ function load_unseen_notification(view = '')
+ {
+  $.ajax({
+   url:"fetch_notification.php",
+   method:"POST",
+   data:{
+       view:view,
+       parent_id : parent_id
+       },
+   dataType:"json",
+   success:function(data)
+   {
+    $('.dropdown-menu').html(data.notification);
+    if(data.unseen_notification > 0)
+    {
+     $('.count').html(data.unseen_notification);
+    }
+   }
+  });
+ }
+ 
+ load_unseen_notification();
+ 
+ $(document).on('click', '.dropdown-toggle', function(){
+  $('.count').html('');
+  load_unseen_notification('yes');
+ });
+ 
+ setInterval(function(){ 
+  load_unseen_notification();; 
+ }, 5000);
+ 
+});
+</script>
+  </body>
 </html>
