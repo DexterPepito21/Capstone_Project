961f7967310d3629c40caea43ea177ef2da282f0
PepitoDexter <imlolilover@gmail.com>
2021-12-19 13:06:56 +0800

    done

--- php/cron.php
+++ php/cron.php
@@ -16,7 +16,7 @@ include("connection.php");
 
 $date = date("Y/m/d", strtotime("+1 day"));
 $today_date =strftime('%Y-%m-%d', strtotime($date));
-echo $today_date.' is the date tommorow <br><br><br>';
+echo $today_date.' is the date tommorow <br><br>';
 $sql = "SELECT * FROM chart";
 $result = mysqli_query($con,$sql);  
 while($rows=mysqli_fetch_assoc($result)){
@@ -34,7 +34,7 @@ if(date("Y/m/d", strtotime($rows['dateofvaccination'])) == $date){
     $result1 = mysqli_query($con,$sql1); 
     $rows1=mysqli_fetch_assoc($result1);
     $number_rows_chart=mysqli_num_rows($result1);
-    echo $chart_id."chart id <br>";
+    // echo $chart_id."chart id <br>";
     $parent_id =  $rows1['parent_id']; // parent id
     $vaccinename = $rows1['vaccinename']; //vaccine name
     $healthcenter = $rows1['healthcenter']; // health center
@@ -49,22 +49,23 @@ if(date("Y/m/d", strtotime($rows['dateofvaccination'])) == $date){
 
     $phonenum =  $rows2['phonenum']; //phone number
 
-    echo $child_id.'child id |';
-    echo $parent_id.'parent id |';
-    echo $phonenum.'phone number ';
-    echo 'date of vaccination is'.$rows['dateofvaccination'].'<br>';
-    echo "Your child is scheduled to have a " .$dose. " dose for " .$vaccinename. " vaccine tomorrow, " .$dateofvaccination. " in Brgy." .$healthcenter. " health center at 8:00 AM. <br>";
-    // start here is the code for sending sms
-    echo $phonenum.'<br>';
+    // echo $child_id.'child id |';
+    // echo $parent_id.'parent id |';
+    // echo $phonenum.'phone number ';
+    // echo 'date of vaccination is'.$rows['dateofvaccination'].'<br>';
+    // echo "Your child is scheduled to have a " .$dose. " dose for " .$vaccinename. " vaccine tomorrow, " .$dateofvaccination. " in Brgy." .$healthcenter. " health center at 8:00 AM. <br>";
+    // echo $phonenum.'<br>';
     
+    // start here is the code for sending sms
     $number = $phonenum;
     $api = "TR-DEXTE784273_EGBWI";
     $pass = "rc[c76\$c18";
     $text = "Your child will have its vaccine tomorrow at 8 AM Please see more details at childcaresystem.online";
-    // $text = $message;
-    //error here
-    // itexmo($number,$text,$api,$pass);
+
+    //Sending message
+    itexmo($number,$text,$api,$pass);
    
+    //Sending notification
     $notif_message = "Your child will have its vaccine tomorrow: ".$dateofvaccination;
     $query = "INSERT INTO comments(comment_subject, comment_text,parent_id)
     VALUES ('vaccine', '$notif_message','$parent_id')";
@@ -75,5 +76,5 @@ if(date("Y/m/d", strtotime($rows['dateofvaccination'])) == $date){
 
 }
 $complete=mysqli_num_rows($result);
-echo $complete;
+echo "Total number of people: ".$complete;
 ?>
